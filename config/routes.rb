e7c8b2306527667181026ce92ce528af7f965ed4
jerrychen <jerrychen040@gmail.com>
2016-09-16 18:35:07 +0800

    let root path wroks.(route.rb->welcome_controller.rb->welcome/index.html.erb)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #[Jerry] set root to go welcome/index page
+  root 'welcome#index'
 end
