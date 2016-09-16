1a5b839b300060b962746ca7c9ac1eb9dbe46b72
jerrychen <jerrychen040@gmail.com>
2016-09-17 01:12:49 +0800

    install devise, install devise Users, and add the authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  #[Jerry] means all controller action(index, new, edit...) in this app need
+  #go the  authenticate_user first!
+  #which means all need to be login or someting authentication
+  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #[Jerry] set root to go welcome/index page
   root 'welcome#index'
