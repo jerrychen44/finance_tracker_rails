efb9c0d804fa7976a8f92c19fd0cf4de9bc264b0
jerrychen <jerrychen040@gmail.com>
2016-09-18 17:21:16 +0800

    add stock.rb class method for modle ORM to use

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,36 @@
 class Stock < ApplicationRecord
+#this is add by stock_quote
+#since we don't have to link with the instance
+#we will define to the class method
+#[Jerry]
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
+
+  def self.new_from_lookup(ticker_symbol)
+    #ref, https://github.com/tyrauber/stock_quote
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    return nil unless looked_up_stock.name
+
+    #create the new model (follow the db column)
+    #check the db/migrate/20160917152810_create_stocks.rb
+    new_stock = new(ticker: looked_up_stock.symbol, name: looked_up_stock.name)
+    new_stock.last_price=new_stock.price
+    new_stock
+  end
+
+  def price
+    #if we have closing price, then return as price
+    closing_price = StockQuote::Stock.quote(ticker).close
+    return "#{closing_price}(closing)" if closing_price
+
+    #if not, we return the opening price as price
+    opening_prince=StockQuote::Stock.quote(ticker).open
+    return "#{opening_prince}(opening)" if opening_prince
+    #if still not have it, return the wording
+    'Unavailable'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #[Jerry] set root to go welcome/index page
   root 'welcome#index'
+  #[Jerry] implement the portfolio page, will need to implement the users controller by myself
+  get 'my_protfolio', to: "users#my_protfolio"
+
 end
