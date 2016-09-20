8057a6b60a09f149d89553e7a11c1cedd6e4712d
jerrychen <jerrychen040@gmail.com>
2016-09-20 09:30:44 +0800

    add the list of user stocks

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -86,7 +86,8 @@ class UserStocksController < ApplicationController
   def destroy
     @user_stock.destroy
     respond_to do |format|
-      format.html { redirect_to user_stocks_url, notice: 'User stock was successfully destroyed.' }
+      #[Jerry] change the page of redirect_to
+      format.html { redirect_to my_portfolio_path, notice: 'User stock was successfully remove from portfolio.' }
       format.json { head :no_content }
     end
   end
